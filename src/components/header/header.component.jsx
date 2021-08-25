5f64295a67c907d26b25d7752fc24dd184b3a7bf
Karina <karina.kuss00@gmail.com>
2021-08-25 23:45:08 +0200

    finished styling header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
-import { Logo, Navbar, Flex, Nav, NavLink, PatreonLink } from './header.styles';
+import React, { useState } from 'react';
+import { Logo, Navbar, Flex, Icon, Nav, NavLink, PatreonLink } from './header.styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
-const Header = () => (
-  <Navbar>
-    <Flex className="wrapper">
-      <Logo>Equestrial</Logo>
-      <Nav>
-        <NavLink to='/'>about</NavLink>
-        <NavLink to='/development'>development</NavLink>
-        <NavLink to='/jobs'>jobs</NavLink>
-        <PatreonLink href='#'>patreon</PatreonLink>
-        <NavLink to='/contact'>contact</NavLink>
-      </Nav>
-    </Flex>
-  </Navbar>
-);
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const toggleMenuOpen = () => {
+    setMenuOpen(!menuOpen);
+  }
+
+  console.log(menuOpen)
+
+  return (
+    <Navbar>
+      <Flex className="wrapper">
+        <Logo>Equestrial</Logo>
+        {menuOpen ? <Icon icon={faTimes} onClick={toggleMenuOpen} /> : <Icon icon={faBars} onClick={toggleMenuOpen} />}
+        <Nav open={menuOpen}>
+          <NavLink to='/'>about</NavLink>
+          <NavLink to='/development'>development</NavLink>
+          <NavLink to='/jobs'>jobs</NavLink>
+          <PatreonLink href='#'>patreon</PatreonLink>
+          <NavLink to='/contact'>contact</NavLink>
+        </Nav>
+      </Flex>
+    </Navbar>
+  )
+};
 
 export default Header;
--- src/components/header/header.styles.jsx
+++ src/components/header/header.styles.jsx
@@ -1,57 +1,132 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const Navbar = styled.div`
   background-color: rgba(0, 0, 0, 0.3);
   position: absolute;
   width: 100%;
   z-index: 99999;
+
+  @media (min-width: 768px) {
+   
+  }
+
+  @media (min-width: 1280px) {
+
+  }
+
 `;
 
+export const Icon = styled(FontAwesomeIcon)`
+  position: absolute;
+  top: 11px;
+  right: 20px;
+  font-size: 2.1em;
+  margin: 13px;
+
+  @media(min-width: 768px) {
+    top: 23px;
+  }
+
+  @media(min-width: 1280px) {
+    display: none;
+  }
+`
+
 export const Flex = styled.div`
-  display: flex;
-  justify-content: space-between;
+  
+  @media (min-width: 1280px) {
+    display: flex;
+    justify-content: space-between;
+  }
 `;
 
 export const Logo = styled.h1`
-  font-size: 3.2em;
+  font-size: 2.1em;
   font-weight: 300;
   letter-spacing: 5px;
   text-transform: uppercase;
+
+  margin-left: 30px;
+
+  @media(min-width: 768px) {
+    font-size: 2.6em;
+  }
+
+  @media(min-width: 1280px) {
+  margin-left: 0;
+  font-size: 3.2em;
+  }
 `;
 
 export const Nav = styled.div`
-   display: flex;
-  justify-content: space-between;
-  margin-top: auto;
-  margin-bottom: auto;
+  display: ${({ open }) => open ? 'block' : 'none'};
 
+  @media(min-width: 768px) {
+
+  }
+
+  @media(min-width: 1280px) {
+    display: flex;
+    justify-content: space-between;
+    margin-top: auto;
+    margin-bottom: auto;
+  }
 `;
 
 export const NavLink = styled(Link)`
   font-size: 1.5em;
   font-weight: 300;
-  margin: 0 25px;
   text-decoration: none;
   color: inherit;
-
-  &:hover {
+    
+    &:hover {
     color: #ff4c29;
   }
-  
-  &:last-child {
-    margin-right: 0;
+
+  // small device
+  display: block;
+  width: 100%;
+  padding: 20px;
+  border-bottom: 1px solid #fff;
+  background-color: rgba(0, 0, 0, 0.7);
+
+  @media(min-width: 1280px) {
+    width: auto;
+    padding: 0;
+    border: none;
+    margin: 0 25px;
+    background-color: transparent;
+
+    &:last-child {
+      margin-right: 0;
+    }
   }
 `;
 
 export const PatreonLink = styled.a`
   font-size: 1.5em;
   font-weight: 300;
-  margin: 0 25px;
   text-decoration: none;
   color: inherit;
 
   &:hover {
     color: #ff4c29;
+  }
+
+  // small device
+  display: block;
+  width: 100%;
+  padding: 20px;
+  border-bottom: 1px solid #fff;
+  background-color: rgba(0, 0, 0, 0.7);
+
+  @media(min-width: 1280px) {
+    width: auto;
+    padding: 0;
+    border: none;
+    margin: 0 25px;
+    background-color: transparent;
   }
 `;
